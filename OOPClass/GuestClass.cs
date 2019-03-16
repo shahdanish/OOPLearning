022b49907d35b18e93ef96f467eff239a8b0a8a3
Danish <shahdanish443@gmail.com>
2019-03-16 22:29:12 +0500

    changes for new learning oop project

---
+++ OOPClass/GuestClass.cs
@@ -0,0 +1,28 @@
+﻿namespace OOPClass
+{
+    public abstract class GuestVist
+    {
+        public string Guestwelcomemessage()
+        {
+            return "welcome guest!";
+        }
+
+        public abstract string purposeofVisit();
+
+    }
+
+    // derived class to inherit the abstract class  
+    public class Houseclass : GuestVist
+    {
+        //static void Main(string[] args)
+        //{
+        //    Houseclass objHouse = new Houseclass();
+        //    objHouse.Guestwelcomemessage();
+        //}
+
+        public override string purposeofVisit()
+        {
+            return "Abstract just came for a Meetup and spend some time ";
+        }
+    }
+}
---
+++ OOPTestApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using OOPClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OOPTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Houseclass hc = new Houseclass();
+            hc.Guestwelcomemessage();
+            hc.purposeofVisit();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
